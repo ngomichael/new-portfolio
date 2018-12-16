0f82ca3a2653d5208767852392a6052049401024
ngomichael <michael_ngo@live.com>
2018-12-15 20:33:58 -0800

    made footer width smaller

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,29 +5,31 @@ class Footer extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <a
-          href="https://github.com/ngomichael"
-          target="_blank"
-          className={styles.link}
-        >
-          GitHub
-        </a>
-        <div className={styles.line} />
-        <a
-          href="https://www.linkedin.com/in/mhngo/"
-          target="_blank"
-          className={styles.link}
-        >
-          LinkedIn
-        </a>
-        <div className={styles.line} />
-        <a
-          href="https://medium.com/@ngomichael"
-          target="_blank"
-          className={styles.link}
-        >
-          Medium
-        </a>
+        <div className={styles.linksContainer}>
+          <a
+            href="https://github.com/ngomichael"
+            target="_blank"
+            className={styles.link}
+          >
+            GitHub
+          </a>
+          <div className={styles.line} />
+          <a
+            href="https://www.linkedin.com/in/mhngo/"
+            target="_blank"
+            className={styles.link}
+          >
+            LinkedIn
+          </a>
+          <div className={styles.line} />
+          <a
+            href="https://medium.com/@ngomichael"
+            target="_blank"
+            className={styles.link}
+          >
+            Medium
+          </a>
+        </div>
       </div>
     )
   }
