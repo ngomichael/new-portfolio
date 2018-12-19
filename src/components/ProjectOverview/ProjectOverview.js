aea738a9f5fdc8a1c71ec2de3f3f02ca18657996
ngomichael <michael_ngo@live.com>
2018-12-18 17:39:43 -0800

    added timeline and type to ProjectOverview

--- src/components/ProjectOverview/ProjectOverview.js
+++ src/components/ProjectOverview/ProjectOverview.js
@@ -3,40 +3,30 @@ import Layout from '../layout'
 import ProjectImage from '../ProjectImage/ProjectImage'
 import TechnologyIcon from '../TechnologyIcon/TechnologyIcon'
 import styles from './ProjectOverview.module.css'
-import aws from '../../icons/aws-icon.svg'
-import docker from '../../icons/docker-icon.svg'
-import golang from '../../icons/golang-icon.svg'
-import mongodb from '../../icons/mongodb-icon.svg'
-import mysql from '../../icons/mysql-icon.svg'
-import mui from '../../icons/mui-icon.svg'
-import node from '../../icons/nodejs-icon.svg'
-import react from '../../icons/react-icon.svg'
-const feStack = stack => stack.map(tech => <li>{tech}</li>)
-const beStack = stack => stack.map(tech => <li>{tech}</li>)
 
-const feTechnologies = [
-  { icon: react, width: '60', margin: '18' },
-  { icon: mui, width: '35', margin: '25' },
-]
-
-const beTechnologies = [
-  { icon: golang, width: '35', margin: '22' },
-  { icon: node, width: '35', margin: '18' },
-  { icon: mysql, width: '40', margin: '26' },
-  { icon: mongodb, width: '35', margin: '24' },
-  { icon: docker, width: '35', margin: '22' },
-  { icon: aws, width: '35', margin: '25' },
-]
+// const feStack = stack => stack.map(tech => <li>{tech}</li>)
+// const beStack = stack => stack.map(tech => <li>{tech}</li>)
 
 class ProjectOverview extends Component {
   render() {
-    console.log(aws)
+    const { overview, feTechnologies, beTechnologies } = this.props
+
     return (
       <div className={styles.container}>
         <h1 className={styles.projectName}>UW Hangouts</h1>
         <div className={styles.overviewContainer}>
+          <div>
+            <div className={styles.projectDetails}>
+              <p className={styles.projectDetailsTitle}>Timeline: </p>
+              <p>{overview.timeline}</p>
+            </div>
+            <div className={styles.projectDetails}>
+              <p className={style.projectDetailsTitle}>Type: </p>
+              <p>{overview.type}</p>
+            </div>
+          </div>
           <h2 className={styles.title}>Overview</h2>
-          <p className={styles.description}>{this.props.overview}</p>
+          <p className={styles.description}>{overview.description}</p>
           <div className={styles.projectImagesContainer}>
             <ProjectImage />
             <ProjectImage />
--- src/pages/hangouts.js
+++ src/pages/hangouts.js
@@ -1,20 +1,46 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import aws from '../icons/aws-icon.svg'
+import docker from '../icons/docker-icon.svg'
+import golang from '../icons/golang-icon.svg'
+import mongodb from '../icons/mongodb-icon.svg'
+import mysql from '../icons/mysql-icon.svg'
+import mui from '../icons/mui-icon.svg'
+import node from '../icons/nodejs-icon.svg'
+import react from '../icons/react-icon.svg'
 import Layout from '../components/layout'
 import ProjectOverview from '../components/ProjectOverview/ProjectOverview'
 import SEO from '../components/seo'
 
-const overview =
-  'UW Hangouts is a project that me and 2 other classmates created for my server-side development class.'
-const feStack = ['React', 'Material UI']
-const beStack = ['Go', 'Node.js', 'MySQL', 'MongoDB', 'WebRTC', 'Docker', 'AWS']
+const overview = {
+  description:
+    'UW Hangouts is a project that me and 2 other classmates created for my server-side development class.',
+  timeline: 'June 2018 - September 2018',
+  type: 'Personal Project',
+}
+const feTechnologies = [
+  { icon: react, width: '60', margin: '18' },
+  { icon: mui, width: '35', margin: '25' },
+]
+
+const beTechnologies = [
+  { icon: golang, width: '35', margin: '22' },
+  { icon: node, width: '35', margin: '18' },
+  { icon: mysql, width: '40', margin: '26' },
+  { icon: mongodb, width: '35', margin: '24' },
+  { icon: docker, width: '35', margin: '22' },
+  { icon: aws, width: '35', margin: '25' },
+]
 const imgs = []
 
 const Hangouts = () => (
   <Layout>
     <SEO title="Hangouts" />
-    <ProjectOverview overview={overview} feStack={feStack} beStack={beStack} />
+    <ProjectOverview
+      overview={overview}
+      feTechnologies={feTechnologies}
+      beTechnologies={beTechnologies}
+    />
     {/* <Link to="/">Go back to the homepage</Link> */}
   </Layout>
 )
