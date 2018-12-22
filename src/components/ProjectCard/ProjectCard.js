902daabcba9b877116046e523d7bf3b1750d0d06
ngomichael <michael_ngo@live.com>
2018-12-21 17:08:15 -0800

    updated projectCard styles

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -31,20 +31,22 @@ class ProjectCard extends Component {
               alt="Macbook Pro"
               height="225"
               width="300"
-              style={{
-                position: 'relative',
-                right: '66px',
-                top: '25px',
-                zIndex: 1,
-              }}
+              // style={{
+              //   position: 'relative',
+              //   right: '66px',
+              //   top: '25px',
+              //   zIndex: 1,
+              // }}
+              className={styles.laptop}
             />
             <img
-              style={{
-                position: 'absolute',
-                right: '198px',
-                top: '56px',
-                zIndex: 1,
-              }}
+              // style={{
+              //   position: 'absolute',
+              //   right: '198px',
+              //   top: '56px',
+              //   zIndex: 1,
+              // }}
+              className={styles.screenshot}
               height="160"
               width="255"
               src={projectInfo.imgs[1]}
