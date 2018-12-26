7038fff6b460f50e7f7956ba0d0ba8444ce3438a
ngomichael <michael_ngo@live.com>
2018-12-25 17:45:07 -0800

    updated header styles

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,28 +6,27 @@ import icon from '../../../public/icons/WebsiteIconSlash.png'
 
 const ListLink = props => {
   return (
-    <Link
-      to={props.to}
-      className={
-        props.currPath === props.to
-          ? `${styles.link} ${styles.blackText}`
-          : `${styles.link} ${styles.grayText}`
-      }
-    >
-      {props.children}
-      {props.to === props.currPath && (
-        <div className={styles.bottomBorder} style={{ width: '50%' }} />
-      )}
-    </Link>
+    <li className={styles.listItem}>
+      <Link
+        to={props.to}
+        className={
+          props.currPath === props.to
+            ? `${styles.link} ${styles.blackText}`
+            : `${styles.link} ${styles.grayText}`
+        }
+      >
+        {props.children}
+        {props.to === props.currPath && (
+          <div className={styles.bottomBorder} style={{ width: '50%' }} />
+        )}
+      </Link>
+    </li>
   )
 }
 
 class Header extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      width: '0',
-    }
   }
 
   render() {
@@ -39,18 +38,20 @@ class Header extends React.Component {
             <img src={icon} />
           </span>
         </Link>
-        <div className={styles.linksContainer}>
+        <ul className={styles.linksContainer}>
           <ListLink to="/" currPath={pathname}>
             Projects
           </ListLink>
-          <a
-            href="https://drive.google.com/file/d/1PyJj4BOHLDTVCYRZOAfWJ72UqeNA6719/view?usp=sharing"
-            target="_blank"
-            className={`${styles.link} ${styles.grayText}`}
-          >
-            Resume
-          </a>
-        </div>
+          <li className={styles.listItem}>
+            <a
+              href="https://drive.google.com/file/d/1PyJj4BOHLDTVCYRZOAfWJ72UqeNA6719/view?usp=sharing"
+              target="_blank"
+              className={`${styles.link} ${styles.grayText}`}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
       </header>
     )
   }
